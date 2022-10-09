cfac65df26743d98067008606fa93a00f705615d
Carlos Alves <carlos.alves.one@gmail.com>
2022-10-09 12:58:30 +0100

    Declare dictionary to score a customer

--- q3_test.py
+++ q3_test.py
@@ -3,5 +3,5 @@
 # Author.....: Carlos Manuel de Oliveira Alves
 # Created...: 09/10/2022
 
-# Declare dictionary with customer data
+# Declare dictionary to score a customer
 customer = {"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"} 
