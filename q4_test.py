1b9cb32ff8aa4b7b50bf766cafea1924a3b4f37b
Carlos Alves <carlos.alves.one@gmail.com>
2022-10-09 13:29:45 +0100

    Import the library requests

--- q4_test.py
+++ q4_test.py
@@ -3,3 +3,9 @@
 # Author.....: Carlos Manuel de Oliveira Alves
 # Created...: 09/10/2022
 
+# Import the library requests
+import requests
+
+url = "YOUR_URL"
+client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+requests.post(url, json=client).json()
