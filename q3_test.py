2cb0eb667045c903ed1ad98d1b98b27514c4abf0
Carlos Alves <carlos.alves.one@gmail.com>
2022-10-09 12:47:50 +0100

    Initial commit, headers and files

---
+++ q3_test.py
@@ -0,0 +1,7 @@
+
+# Machine Learning ZoomCamp - Homework 5
+# Author.....: Carlos Manuel de Oliveira Alves
+# Created...: 09/10/2022
+
+# Declare dictionary with customer data
+customer = {"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"} 
