09ef71fdbfcddfd8f1aa253f0d9777490d595122
Carlos Alves <carlos.alves.one@gmail.com>
2022-10-09 13:44:38 +0100

    Import the library Flask and some packages

--- q4_predict.py
+++ q4_predict.py
@@ -3,3 +3,20 @@
 # Author.....: Carlos Manuel de Oliveira Alves
 # Created...: 09/10/2022
 
+# Import the library Flask and some packages
+from flask import Flask
+from flask import request
+from flask import jsonify # use this package to connect dictionary to JSON
+
+# Import pickle library to load files
+import pickle
+
+# Create function for loading the pickle files
+def load(filename):
+    with open(filename, 'rb') as f_in:
+        return pickle.load(f_in)
+
+# Load and store the files dictionary vectorizer and the model
+dv = load('dv.bin')
+model = load('model1.bin')
+
