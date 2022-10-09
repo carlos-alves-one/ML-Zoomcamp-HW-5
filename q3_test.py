922a0729e1724f13534ac6813a5ee74f52120576
Carlos Alves <carlos.alves.one@gmail.com>
2022-10-09 13:00:03 +0100

    Import pickle library to load files

--- q3_test.py
+++ q3_test.py
@@ -3,5 +3,8 @@
 # Author.....: Carlos Manuel de Oliveira Alves
 # Created...: 09/10/2022
 
+# Import pickle library to load files
+import pickle
+
 # Declare dictionary to score a customer
 customer = {"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"} 
